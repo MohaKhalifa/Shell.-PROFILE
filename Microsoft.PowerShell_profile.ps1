3ec1326707be0a3339b28dc92ea4f1886a47fef2
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-10-10 17:48:30 -0500

    making the band

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+function sl {clear}
+function p { Set-Location .. ; clear; Get-ChildItem;}
+function x {exit}
+function cl { clear; Get-ChildItem}
+function home {cd ~}
+
+
+function hub {cd ~\gh}
+function vim {Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Vim 9.1\Vim.lnk" }
+
+
+function lcommit {git init; git add ./*; git commit -m "making the band"; git push;}
+function lpull {git inti; git pull;}
+function shellem {Copy-Item -Path C:\Users\mohat\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Destination C:\Users\mohat\gh\SP\; cd C:\Users\mohat\gh\SP;  lcommit;}
