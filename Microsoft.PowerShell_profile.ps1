bf20746af4c91598577add92c7d6a3d6213ba4ac
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-10-10 20:13:48 -0500

    making the band

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ function p { Set-Location .. ; clear; Get-ChildItem;}
 function x {exit}
 function cl { clear; Get-ChildItem}
 function home {cd ~}
-function offme {Stop-Computer}
+function offme {Stop-Computer -Force}
 
 
 function hub {cd ~\gh}
