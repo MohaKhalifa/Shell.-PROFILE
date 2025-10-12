d130120d21a5a89cb4d806e6bcf36dea6b8e5d90
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-10-12 06:21:17 -0500

    making the band

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,7 +13,7 @@ function vim {saps "C:\Program Files\Vim\vim91\vim.exe" }
 
 function lcommit {git init; git add ./*; git commit -m "making the band"; git push;}
 function lpull {git inti; git pull;}
-function shellem {Copy-Item -Path $PROFILE -Destination ~\gh\SP\; cd ~\gh\SP;  lcommit;home; . $PROFILE}
+function shellem {Copy-Item -Path $PROFILE -Destination ~\gh\SP\; cd ~\gh\SP;  lcommit; cd ~; clear; . $PROFILE}
 function shellme {Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Vim 9.1\Vim.lnk" $PROFILE}
 function wimip {curl ifconfig.me/ip | Select-Object Content}
 <# https://powershellcommands.com/change-powershell-color #>
