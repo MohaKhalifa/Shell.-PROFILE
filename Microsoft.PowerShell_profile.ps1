b53b413534bcce148108c19095ee1930d4279d12
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-10-12 06:15:32 -0500

    making the band

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -15,6 +15,6 @@ function lcommit {git init; git add ./*; git commit -m "making the band"; git pu
 function lpull {git inti; git pull;}
 function shellem {Copy-Item -Path $PROFILE -Destination ~\gh\SP\; cd ~\gh\SP;  lcommit; & $PROFILE}
 function shellme {Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Vim 9.1\Vim.lnk" $PROFILE; home}
-function wimip {curl ifconfig.me/ip}
+function wimip {curl ifconfig.me/ip | Select-Object Content}
 <# https://powershellcommands.com/change-powershell-color #>
 <#Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0#>
