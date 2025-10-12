f810d68d680ec1cc8b6e32a3a4d9065ad848b045
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-10-12 05:36:57 -0500

    making the band

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@ function xl {Clear-Host}
 function p { Set-Location .. ; clear; Get-ChildItem;}
 function x {exit}
 function cl { clear; Get-ChildItem}
-function home {cd ~}
+function home {cd ~; clear}
 function offme {Stop-Computer -Force}
 
 
@@ -12,6 +12,6 @@ function vim {saps "C:\Program Files\Vim\vim91\vim.exe" }
 
 function lcommit {git init; git add ./*; git commit -m "making the band"; git push;}
 function lpull {git inti; git pull;}
-function shellem {Copy-Item -Path $PROFILE -Destination ~\gh\SP\; cd ~\gh\SP;  lcommit; home; clear}
+function shellem {Copy-Item -Path $PROFILE -Destination ~\gh\SP\; cd ~\gh\SP;  lcommit; home}
 function shellme {Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Vim 9.1\Vim.lnk" $PROFILE}
 <# https://powershellcommands.com/change-powershell-color #>
