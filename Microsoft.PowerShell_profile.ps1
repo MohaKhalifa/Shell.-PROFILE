01a1c02f7a225db74584a7d6a14471a5191398fe
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-11-01 16:12:56 +0100

    making the band

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,11 +4,34 @@ function x {exit}
 function cl { clear; Get-ChildItem}
 function home {cd ~; clear}
 function offme {Stop-Computer -Force}
+function book {cd ~\audioBooks; clear}
 
 
 function hub {cd ~\gh; cl}
 function man {Get-Help -Name}
 <# function vim {saps "C:\Program Files\Vim\vim91\vim.exe" }// ?pid #>
+function makeGithubWebpage {git init; cd ~\gh;$amiin = gh auth status | Select-Object "Logged in to githu.com account MohaKhalifa";
+if ($amiin = $null ) 
+{
+gh auth login
+	}
+else{
+echo "we making a webpage via and files will be uploaded to github.";
+git init;
+echo "" >>index.html;
+echo "" >>index.js;
+echo "" styles.css;
+$companyName Read-Host 'Company name';
+$currentDateForPage = Get-Date;
+$commity = echo "A webpage for $companyName created on $currentDateForPage" >> README.md;
+git add .\*;
+git commit -m "$commity";
+git add origin main
+git push;
+	}
+
+}
+<#function testit {$timy = Read-Host 'what time do think it is right now'; echo $timy}#>
 
 
 function lcommit {git init; git add ./*; git commit -m "making the band"; git push;}
