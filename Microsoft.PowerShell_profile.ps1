5b375ace3843b08da999dd6a1b1bab980dd3e982
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-11-02 22:13:17 +0100

    making the band

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -59,7 +59,7 @@ Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Vim 9.1\Vim.
 function lcommit {git init; git add ./*; git commit -m "making the band"; git push;}
 function lpull {git init; git pull;}
 function shellem {Copy-Item -Path $PROFILE -Destination ~\gh\SP\; cd ~\gh\SP;  lcommit; cd ~; clear; x}
-function shellme {Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Vim 9.1\Vim.lnk" $PROFILE}
+function shellme {Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Vim 9.1\Vim.lnk" $PROFILE; clear}
 function wimip {curl ifconfig.me/ip | Select-Object Content}
 function vscode {  $nameof = pwd; Start-Process  "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk" $nameof}
 
