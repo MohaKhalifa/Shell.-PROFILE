78af8933b2a1673e00eebe2928ae694a22e85e7e
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-11-09 14:59:07 +0100

    11/09/2025 14:59:07

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,8 +7,13 @@ $curdate = Get-Date;
 start interactive session in ps; argumental program
 
 TD:
+A:______: DL:Novemeber 30th the basic: finale Project ToBD 
 Create an extention for definitions using duckduckgo search make it lightweight add event listener for selected items;
 make sure no conflictions are made; beware vimium and ytstuff upload on chrome webstore and ff
+add history, remiders if possible;
+use apis if possible; make accs or local files easily syncronizable;
+idnetify articles level using llm and scope hard words; highlit the words you had in the past;
+market the stuff; project
 
 
 appearantly the value of assigned to items gets stored when the shell session starts ///conclution if for loop inefficent that's why you need to put them inside functions BigBrainOnBradFR#>
