b1dc6097d37ba34a062ed61d63e3585a4036f8dd
mohakhalifa <04mohamedkhalifa04@gmail.com>
2026-01-02 22:38:02 +0100

    01/02/2026 22:38:02

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -64,7 +64,7 @@ function cl { clear; Get-ChildItem}
 function home {cd ~; clear}
 function offme {Stop-Computer -Force}
 function book {cd ~\audioBooks; clear}
-function proj {cd ~\Desktop\AOSAV;clear; ls}
+function proj {cd ~\Desktop\GraduationProject ;clear; ls}
 
 function hub {cd ~\gh; cl}
 function man {Get-Help -Name}
